c0f071b76b510fc0ba3f907486a918e3ebd6a40e
uthokala <uttamredddy861@gmail.com>
2025-03-12 22:20:47 -0500

    added

---
+++ task_json_iframe/script.js
@@ -0,0 +1,22 @@
+const works = {
+    "task1": { "title": "Project 1", "url": "works/task1/index.html" },
+    "task2": { "title": "Project 2", "url": "works/task2/index.html" },
+    "task3": { "title": "Project 3", "url": "works/task3/index.html" },
+    "task4": { "title": "Project 4", "url": "works/task2/index.html" },
+    "task5": { "title": "Project 5", "url": "works/task1/index.html" },
+};
+
+const navDiv = document.getElementById("nav_div");
+const contentFrame = document.getElementById("content_frame");
+
+// Create navigation links dynamically
+for (let key in works) {
+    let navItem = document.createElement("div");
+    navItem.classList.add("nav_item");
+    navItem.innerText = works[key].title;
+    navItem.onclick = function() {
+        contentFrame.src = works[key].url;
+    };
+    navDiv.appendChild(navItem);
+}
+
